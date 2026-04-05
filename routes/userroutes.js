e9f5a55a93c7f36fb39ad903e4d8842ff9618b20
Sudhir Mathur <22BCS50108@cuchd.in>
2026-04-05 20:15:19 +0530

    Naming Improvemnt

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const router = express.Router();
-
 const {
   createUser,
   loginUser,
   updateUserStatus
 } = require("../controllers/userController");
-
 const auth = require("../middleware/auth");
 const checkRole = require("../middleware/role");
-
-// ✅ FIXED ROUTES
 router.post("/register", createUser);
 router.post("/login", loginUser);
 router.patch("/:id/status", auth, checkRole("admin"), updateUserStatus);
