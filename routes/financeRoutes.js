19cc0275746789f53fcaa273049648a170a2e296
Sudhir Mathur <22BCS50108@cuchd.in>
2026-04-05 20:19:42 +0530

    Update financeRoutes.js

--- routes/financeRoutes.js
+++ routes/financeRoutes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-
 const {
   createFinance,
   getAllFinance,
@@ -9,26 +8,12 @@ const {
   updateFinance,
   deleteFinance
 } = require("../controllers/financeController");
-
 const auth = require("../middleware/auth");
 const checkRole = require("../middleware/role");
-
-// CREATE
 router.post("/", auth, checkRole("admin", "viewer"), createFinance);
-
-// GET ALL
 router.get("/", auth, getAllFinance);
-
-// SUMMARY
 router.get("/summary", auth, getSummary);
-
-// CATEGORY SUMMARY
 router.get("/category-summary", auth, getCategorySummary);
-
-// UPDATE
 router.put("/:id", auth, checkRole("admin"), updateFinance);
-
-// DELETE
 router.delete("/:id", auth, checkRole("admin"), deleteFinance);
-
 module.exports = router;
