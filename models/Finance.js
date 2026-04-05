90247113ef54ff678acdfbb62e39f3a17fb71e2b
Sudhir Mathur <22BCS50108@cuchd.in>
2026-04-05 20:21:42 +0530

    Update Finance.js

--- models/Finance.js
+++ models/Finance.js
@@ -1,32 +1,24 @@
 const mongoose = require("mongoose");
-
 const financeSchema = new mongoose.Schema({
   amount: { type: Number, required: true },
-
   type: {
     type: String,
     enum: ["income", "expense"],
     required: true
   },
-
   category: { type: String, required: true },
-
   date: {
     type: Date,
     default: Date.now   // अगर नहीं दिया तो current date
   },
-
-  note: {
+note: {
     type: String,
     default: ""
   },
-
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true
   }
-
 }, { timestamps: true });
-
 module.exports = mongoose.model("Finance", financeSchema);
